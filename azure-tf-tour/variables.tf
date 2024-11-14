0ce78a88d444490087acd95fdebb409842d576e2
joshakumah <joshua.akumah@gmail.com>
2024-11-14 17:22:18 +0000

    terraform and Azure

---
+++ azure-tf-tour/variables.tf
@@ -0,0 +1,15 @@
+variable "build_location" {
+  type = string
+  default = "westus2"
+}
+
+# this is now an "input" value
+variable "rg_name" {
+  type = string
+  default = "myAzureRG"
+}
+
+variable "name" {
+  type = string
+  default = "myTFVnet"
+}
